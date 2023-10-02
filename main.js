16e7ce085b601348091378dbbd2d7c5f5bf8f507
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2023-10-02 22:53:32 +0000

    tokenizing done

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 
-const fs=require('fs')
-const path=require('path')
-var tokenMap={
-    "==":"=",
-    "&&":"and",
-    "||":"or",
-    "!":"not",
-    "^":"xor",
-    "{":"then",
-    "}":"end",
-    "=":"->",
-    "true":"1",
-    "false":"0",
-    "var":""
+const fs = require('fs')
+const path = require('path')
+const tokenMap = {
+    "==": "=",
+    "&&": "and",
+    "||": "or",
+    "!": "not",
+    "^": "xor",
+    "{": "then",
+    "}": "end",
+    "=": "->",
+    "true": "1",
+    "false": "0",
+    "var": "",
+    "[": "{",
+    "]": "}"
 }
 function indexOf(searchStr, str, caseSensitive) {
     var searchStrLen = searchStr.length;
@@ -45,56 +47,80 @@ function parseArgs(keys) {
     //console.log(res)
     return res
 }
-function parse(file){
-    file =file.toString()
-    function Util(){}
-    Util.prototype.inString=function(char){
-        var ret=false
-        if(!this.stringMap){
+function parse(file) {
+    file = file.toString()
+    function Util() { }
+    Util.prototype.inString = function (char) {
+        var ret = false
+        if (!this.stringMap) {
             this.genStrMap()
         }
-        this.stringMap.forEach((item)=>{
+        this.stringMap.forEach((item) => {
             try {
-                console.log(char>=item[0],char<=item[1])
-                if((char>=item[0])&&(char<=item[1])){
-                    ret =true
+                //console.log(char >= item[0], char <= item[1])
+                if ((char >= item[0]) && (char <= item[1])) {
+                    ret = true
                 }
             } catch (error) {
-                
+
                 return
             }
-            
+
         })
-        
+
         return ret
     }
-    Util.prototype.genStrMap=function(){
-        this.stringMap=[]
-        var strs=indexOf('"',file,true)
-        strs.forEach((section,i)=>{
-            
-            if((i%2==0)){
-                this.stringMap.push([strs[i],strs[i+1]])
+    Util.prototype.genStrMap = function () {
+        this.stringMap = []
+        var strs = indexOf('"', file, true)
+        strs.forEach((section, i) => {
+
+            if ((i % 2 == 0)) {
+                this.stringMap.push([strs[i], strs[i + 1]])
             }
         })
-        
+
     }
-    var locations ={}
-    var util=new Util()
-    Object.keys(tokenMap).forEach((token)=>{
-        var tokenList=indexOf(token,file,true)
-        locations[token]=[]
-        tokenList.forEach((pos)=>{
-            if(!util.inString(pos)){
-                locations[token].push(pos)
-            }
+    Util.prototype.tokenize = function (tokenMap, file) {
+        var locations = {}
+        Object.keys(tokenMap).forEach((token) => {
+            var tokenList = indexOf(token, file, true)
+            locations[token] = []
+            tokenList.forEach((pos) => {
+                if (!this.inString(pos)) {
+                    locations[token].push(pos)
+                }
+            })
         })
-    })
-    console.log(locations,util.stringMap)
+        return locations
+    }
+    Util.prototype.fixTokens=function(tokenPos,fileStr){
+        const fixable=["var"]
+        
+        Object.keys(tokenPos).forEach(function(tokenVal,c){
+            //console.log(tokenVal)
+            
+            tokenPos[tokenVal].forEach(function(token){
+                //console.log(tokenVal)
+                //console.log(fileStr)
+                if(fixable.includes(Object.values(tokenVal)[c])){
+                    console.log(fileStr)
+                    var seq=fileStr.split("").slice(token)
+                    console.log(seq)
+                }
+            })
+            
+        })
+    }
+    var util = new Util()
+    const tokenPos = util.tokenize(tokenMap,file)
+    
+    var tokens = util.fixTokens(tokenPos,file)
+    
 }
 
-function cli(){
-    var args = parseArgs({flags:[],options:["file"]})
-    parse(fs.readFileSync(path.join(__dirname,args.file)))
+function cli() {
+    var args = parseArgs({ flags: [], options: ["file"] })
+    parse(fs.readFileSync(path.join(__dirname, args.file)))
 }
 cli()
