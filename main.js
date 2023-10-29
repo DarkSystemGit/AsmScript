4d5c56270aebed92f18a9527236130542d5dd23a
Dark System <darksystemcoder@gmail.com>
2023-10-28 23:59:33 -0400

    visitor class

--- main.js
+++ main.js
@@ -5,14 +5,12 @@ import ICEScriptLexer from "./antlr/parsers/antlr/grammars/ICEScriptLexer.js";
 import tokens from "./tokens.js";
 import {readFileSync,writeFileSync} from 'fs'
 //console.log(readFileSync(process.argv[2]))
-const chars = new antlr4.InputStream(readFileSync(process.argv[2]).toString());
-const lexer = new ICEScriptLexer(chars);
-const tokenstr = new antlr4.CommonTokenStream(lexer);
-const parser = new ICEScriptParser(tokenstr);
-const tree = parser.script();
+
 function handler(token,ctx){
     var handlers={
-        "&":"and"
+        "&":"and",
+        "||":"or",
+        "!!":"not"
     }
     if(handlers.hasOwnProperty(token)){
         if(typeof handlers[token]=="Function"){
@@ -22,4 +20,28 @@ function handler(token,ctx){
         }
     }
 }
-console.log(tree.toStringTree(parser.ruleNames))
+class Visitor extends ICEScriptVisitor{
+
+    visitChildren(ctx) {
+        console.log(ctx.getText())
+        let code = '';
+
+        for (let i = 0; i < ctx.getChildCount(); i++) {
+            code += this.visit(ctx.getChild(i));
+        }
+
+        return code.trim();
+    }
+    start(ctx) {
+        return this.visitExpressionSequence(ctx);
+    }
+    visitTerminal(ctx) {
+        return ctx.getText();
+    }
+}
+//console.log(tree.toStringTree(parser.ruleNames))
+const chars = new antlr4.InputStream(readFileSync(process.argv[2]).toString());
+const lexer = new ICEScriptLexer(chars);
+const tokenstr = new antlr4.CommonTokenStream(lexer);
+const parser = new ICEScriptParser(tokenstr);
+const tree = parser.script();
