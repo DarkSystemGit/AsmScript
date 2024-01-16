ddfbde8ac37166cbaf489d43287a7ce3759f5580
DarkSystemGit <pdarksystem@gmail.com>
2024-01-16 18:28:58 -0500

    nodes

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-import { buildAst } from "./src/antlr.js"
+import { parse } from "./src/parser/parser.js"
 import * as fs from 'fs' 
 import * as path from 'path'
 import * as util from './src/util.js'
 import * as yaml from 'yaml'
 util.info('Glacier Compiler v1.0.0\n'+`Creating Ast for ${path.basename(process.argv[2])}...`)
 util.write('./log','{}')
-buildAst(path.join(process.cwd(),process.argv[2]))
+parse(path.join(process.cwd(),process.argv[2]))
 //console.dir(util.data.asts,{depth:null})
 util.write('./ast.json',JSON.stringify(util.data.asts,null,2))
-if(process.env.DEBUG=="true"){
+/*if(process.env.DEBUG=="true"){
     var doc =new yaml.Document()
     doc.contents=util.data.asts
     util.write('./ast.yaml',doc.toString())
-}
+}*/
 util.info('Done!')
---
+++ src/parser/nodes/generateNodes.js
@@ -0,0 +1,8 @@
+import {readFileSync,writeFileSync} from 'fs'
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+JSON.parse(readFileSync(path.join(__dirname,'nodes.json'))).forEach((elm)=>{
+    writeFileSync(path.join(__dirname,elm.replace('visit','')+'.js'),`export default (elm,parser)=>{
+    }`)
+})
