d107a735b7a227fb941ecfcaa71dd23cfead706d
DarkSystemGit <pdarksystem@gmail.com>
2024-02-12 18:09:42 -0500

    binary expresions

--- src/parser/nodes/BinaryExpression.js
+++ src/parser/nodes/BinaryExpression.js
@@ -1,5 +1,25 @@
 import * as util from './../../util.js'
-export default (elm,parser)=>{
-    
-    return {node:""}
+export default (elm, parser) => {
+    var node = { children: [parser(elm.left), parser(elm.right)], operation: elm.operator }
+    var op = (op) => {
+        var res = false
+        op.forEach(elm => {
+            if(elm.type==elm)res=true
+        })
+        return res
+    }
+    if (op(['+','-','/','*','%','**'])) {
+        node.type='number'
+        node.node='arthmeticOp'
+    }else if(op(['==','=','!=','!==','<=','<','>','>='])){
+        node.type='bool'
+        node.node='comparison'
+    }else if(op(['&','^','|'])){
+        node.type='binary'
+        node.node='bitwise'
+    }else if(op(['||','&&'])){
+        node.node='logic'
+        node.type='bool'
+    }
+    return node
 }
